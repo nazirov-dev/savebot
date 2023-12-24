d8889a70f32ffe55454e69c41570ec65ea2ee84b
nazirov-dev <nazirovdev@gmail.com>
2023-12-24 22:41:56 +0500

    .

--- app/Http/Controllers/InstagramDownloader.php
+++ app/Http/Controllers/InstagramDownloader.php
@@ -88,7 +88,7 @@ class InstagramDownloader extends Controller
                             if($media['media_type'] == 1) {
                                 $type = 'photo';
                                 $url = $media['image_versions2']['candidates'][0]['url'];
-                            } elseif($media['media_type'] == 2 && $media['has_audio']) {
+                            } elseif($media['media_type'] == 2) {
                                 $type = 'video';
                                 $url = $media['video_versions'][0]['url'];
                             } else {
