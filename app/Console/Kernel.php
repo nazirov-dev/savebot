0ba3e3fa67a35c96a0c6660b517a0da2030f78fe
nazirov-dev <nazirovdev@gmail.com>
2023-12-27 19:37:32 +0500

    Send notification schedule added

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -13,6 +13,7 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule): void
     {
 
+        $schedule->command('app:send-notification')->everyMinute();
         $schedule->command('app:clean-files')->everyFiveMinutes();
         // $schedule->command('inspire')->hourly();
     }
